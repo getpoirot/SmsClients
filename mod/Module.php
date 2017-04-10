007240ffcab4252a3336980144108c301679f293
Payam Naderi <naderi.payam@gmail.com>
2017-04-10 10:44:27 +0430

    init sms clients as module

---
+++ mod/Module.php
@@ -0,0 +1,12 @@
+<?php
+namespace Module\SmsClients;
+
+use Poirot\Application\Interfaces\Sapi;
+
+
+class Module implements Sapi\iSapiModule
+{
+    const CONF_KEY = 'module.smsclients';
+
+    
+}
